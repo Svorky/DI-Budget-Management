9b3dd35df0517a6c948a408573f37a68253a409a
alexanderbaytman <alexanderbaytman.menow@gmail.com>
2024-07-25 12:22:27 +0300

    forms

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import { expensesRouter } from './routes/expensesRoute.js'
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -13,3 +17,7 @@ app.listen(5000, () => {
 })
 
 app.use('/api/expenses', expensesRouter)
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
